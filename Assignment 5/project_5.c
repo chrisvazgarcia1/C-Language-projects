c29390edd208ee09970bbc2cb71c9409d2abf9c5
Christian Vazquez <122330835+chrisvazgarcia1@users.noreply.github.com>
2024-10-26 03:51:20 -0500

    Update project_5.c

--- Assignment 5/project_5.c
+++ Assignment 5/project_5.c
@@ -2,27 +2,89 @@
 
 int main (void) {
 	
-	int maxPoints;
+	int pointsPossible;
+	int totalPointsPossible;
 	int CURR_ASSIGNMENT = 50;
+
+	char percent = '%';
 	
-	float totalPoints;
 	float testPoints[CURR_ASSIGNMENT];
+	float totalPoints = 0;
+	float pointPercentage;
 	
-	printf("\nPlease enter points possible per assignment: ");
+	printf("\nPlease enter the points possible per assignment: ");
+
+	CURR_ASSIGNMENT = 0;
+
+	if (scanf("%d", &pointsPossible)){
+
+		totalPointsPossible = pointsPossible;
 	
-	scanf("%d", &maxPoints);
-	
-	totalPoints = 0;
-	CURR_ASSIGNMENT = 1;
-	
-	while (scanf("\n%lf", &testPoints[CURR_ASSIGNMENT])) {
-		printf("Please enter assignment %d grade [q to quit]: ", CURR_ASSIGNMENT);
+	printf("Please enter assignment %d grade [q to quit]: ", CURR_ASSIGNMENT + 1);
+
+
+		while (scanf("%f", &testPoints[CURR_ASSIGNMENT])) {
+			if (testPoints[CURR_ASSIGNMENT] > pointsPossible){
+				printf("Error: Grade higher than possible grade for the assignment.");
+			}
+			else {
+				totalPoints = totalPoints + testPoints[CURR_ASSIGNMENT];
+				CURR_ASSIGNMENT++;		
+			}
+
+			printf("Please enter assignment %d grade [q to quit]: ", CURR_ASSIGNMENT + 1);
+
+		}
+
+		int CURR_COUNT;
+
+		printf("You entered %d assignment grades as follows:", CURR_ASSIGNMENT);
+
+		for (CURR_COUNT = CURR_ASSIGNMENT - 1; CURR_COUNT >= 0; --CURR_COUNT){
+
+			pointPercentage = (testPoints[CURR_COUNT] / pointsPossible) * 100;
+
+			printf("\nAssignment %d grade was %1.f points out of %d : %1.f%c - ", CURR_COUNT + 1, testPoints[CURR_COUNT], pointsPossible, pointPercentage, percent);
+
+			if (pointPercentage > 89) {
+			printf("A");
+			}
+			else if (pointPercentage > 79) {
+				printf("B");
+			}
+			else if (pointPercentage > 69) {
+				printf("C");
+			}
+			else if (pointPercentage > 59){
+				printf("F");
+			}
+			else {
+				printf("D");
+			}
+		}
+
+		totalPointsPossible = pointsPossible * CURR_ASSIGNMENT;
+
+		pointPercentage = (totalPoints / totalPointsPossible) * 100;
 		
-		totalPoints += testPoints[CURR_ASSIGNMENT];
-		++CURR_ASSIGNMENT;		
+
+		printf("\n%1.f points out of %d possible for an overall grade of %1.f%c - ", totalPoints, totalPointsPossible, pointPercentage, percent);
+		if (pointPercentage > 89) {
+			printf("A");
+		}
+		else if (pointPercentage > 79) {
+			printf("B");
+		}
+		else if (pointPercentage > 69) {
+			printf("C");
+		}
+		else if (pointPercentage > 59){
+			printf("F");
+		}
+		else {
+			printf("D");
+		}
+	
 	}
-	
-	printf("%0.2f", totalPoints);
-	
 	return 0;
 }	
